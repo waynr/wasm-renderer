b61cb6eee1325dd2964638ed0a9c97156e9f1cb5
wayne warren <wayne.warren.s@gmail.com>
2023-05-02 21:01:17 -0600

    wip: very janky implementation of AsRef<[u8]> for Frame

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::sync::{Arc, Mutex};
+use std::marker::PhantomData;
+use std::ops::Deref;
+use std::ptr::NonNull;
+use std::sync::atomic;
+use std::sync::atomic::Ordering;
+use std::sync::Mutex;
 
 use iced::widget::{column, container, image, text};
 use iced::{executor, Application, Command, Element, Settings, Theme};
@@ -14,7 +19,6 @@ struct WasmDemoRunner {
     height: u32,
     bytes_required: u64,
 
-    frame: Arc<Frame>,
     frame_manager: FrameManager,
 }
 
@@ -30,7 +34,13 @@ impl FrameManager {
         Self {
             size,
             last_updated: None,
-            frames: vec![Frame::new(size); 10],
+            frames: vec![
+                Frame::new(size),
+                Frame::new(size),
+                Frame::new(size),
+                Frame::new(size),
+                Frame::new(size),
+            ],
         }
     }
 
@@ -38,59 +48,122 @@ impl FrameManager {
         let frame = self
             .frames
             .iter_mut()
-            .find(|f| Arc::strong_count(&f.0) == 1)
+            .find(|f| Frame::count(&f) == 1)
             .ok_or("couldn't find free frame")?;
 
         Ok(frame.clone())
     }
 }
 
-#[derive(Clone, Debug)]
-struct Frame(Arc<Mutex<InnerFrame>>);
+#[derive(Debug)]
+struct Frame {
+    ptr: NonNull<InnerFrame>,
+    phantom: PhantomData<InnerFrame>,
+}
 
+// following the rustinomicon guide for implementing Arc: https://doc.rust-lang.org/nomicon/arc-mutex/arc-base.html
+//
+// the goal is to satisfy the constraints on image::Handle::from_pixels:
+//      impl AsRef<[u8]> + Send + Sync + 'static,
+// unfortunately I can't just wrap a Vec<u8> in Arc<Mutex<T>> because of the AsRef<[u8]> constraint
+// and I haven't been able to figure out how to return &[u8] from a type protected by Arc<Mutex<T>>
+//
+// this is ultimately intended to serve the purpose of not allocating a new Vec<u8> every time i
+// want to pass a wasm-generated pixel buffer to the iced library
 impl Frame {
     fn new(size: usize) -> Self {
-        Self(Arc::new(Mutex::new(InnerFrame::new(size))))
+        let boxed = Box::new(InnerFrame {
+            // the reference count starts here at 1 since this is the first pointer to this new
+            // data
+            rc: atomic::AtomicUsize::new(1),
+            buf: vec![0; size as usize],
+            lock: Mutex::new(()),
+        });
+
+        Self {
+            // `.unwrap()` is okay here since the pointer returned by `Box::into_raw` is guaranteed
+            // not to be null
+            ptr: NonNull::new(Box::into_raw(boxed)).unwrap(),
+            phantom: PhantomData,
+        }
+    }
+
+    fn count(this: &Self) -> usize {
+        this.inner().rc.load(Ordering::Acquire)
+    }
+
+    fn inner(&self) -> &InnerFrame {
+        unsafe { self.ptr.as_ref() }
     }
 
     fn copy_from_memory(
-        self,
+        &mut self,
         view: MemoryView,
     ) -> std::result::Result<(), Box<dyn std::error::Error>> {
-        let mut binding = match self.0.lock() {
-            Ok(inner) => inner,
-            Err(_e) => return Err("whoops".into()),
-        };
-        let mut s = binding.buf.as_mut_slice();
-        view.read(0, &mut s)?;
+        let inner = unsafe { self.ptr.as_mut() };
+        let _guard = inner.lock.lock()?;
+        view.read(0, inner.buf.as_mut_slice())?;
         Ok(())
     }
 }
 
-#[derive(Debug)]
-struct InnerFrame {
-    buf: Vec<u8>,
+// Frame is Send because access to mutable state is enforced internally with an atomic reference
+// count.
+unsafe impl Send for Frame {}
+// Frame is Sync because we ensure nothing stored in a &Frame can be written to while that same
+// thing could be read or written to from another &Frame -- enforced using atomic reference count.
+unsafe impl Sync for Frame {}
+
+impl Deref for Frame {
+    type Target = [u8];
+
+    fn deref(&self) -> &[u8] {
+        let inner = unsafe { self.ptr.as_ref() };
+        &inner.buf.as_slice()
+    }
 }
 
-impl InnerFrame {
-    fn new(size: usize) -> Self {
-        Self {
-            buf: vec![0; size as usize],
+impl Clone for Frame {
+    fn clone(&self) -> Self {
+        let inner = unsafe { self.ptr.as_ref() };
+
+        // relaxed ordering is okay here since we don't need to modify or access the inner data and
+        // therefore don't need atomic synchronization
+        let old_rc = inner.rc.fetch_add(1, Ordering::Relaxed);
+
+        if old_rc >= isize::MAX as usize {
+            std::process::abort();
         }
+
+        Self {
+            ptr: self.ptr,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl Drop for Frame {
+    fn drop(&mut self) {
+        let inner = unsafe { self.ptr.as_ref() };
+        if inner.rc.fetch_sub(1, Ordering::Release) != 1 {
+            return;
+        }
+        atomic::fence(Ordering::Acquire);
+        unsafe { Box::from_raw(self.ptr.as_ptr()) };
     }
 }
 
 impl AsRef<[u8]> for Frame {
     fn as_ref(&self) -> &[u8] {
-        match self.0.lock() {
-            Ok(f) => {
-                f.buf.as_slice()
-            },
-            Err(_) => {
-                panic!()
-            },
-        }
+        &self
     }
+}
+
+#[derive(Debug)]
+struct InnerFrame {
+    lock: Mutex<()>,
+    rc: atomic::AtomicUsize,
+    buf: Vec<u8>,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -138,7 +211,6 @@ impl Application for WasmDemoRunner {
             width: width as u32,
             height: height as u32,
             bytes_required,
-            frame: Arc::new(Frame::new(bytes_required as usize)),
             frame_manager: FrameManager::new(bytes_required as usize),
         };
         runner.tick();
@@ -161,8 +233,7 @@ impl Application for WasmDemoRunner {
     fn view(&self) -> Element<Self::Message> {
         let center: Element<Self::Message> = match &self.frame_manager.last_updated {
             Some(frame) => {
-                let image_handle =
-                    image::Handle::from_pixels(self.width, self.height, frame.clone());
+                let image_handle = image::Handle::from_pixels(self.width, self.height, frame.clone());
                 image::Viewer::new(image_handle)
                     .width(self.width as f32)
                     .height(self.height as f32)
@@ -177,6 +248,7 @@ impl Application for WasmDemoRunner {
 
 impl WasmDemoRunner {
     fn tick(&mut self) -> std::result::Result<(), Box<dyn std::error::Error>> {
+        self.frame_manager.last_updated = None;
         let tick = self
             .module_instance
             .exports
@@ -187,13 +259,14 @@ impl WasmDemoRunner {
             .call(&mut self.wasm_store, vec![].as_slice())
             .expect("calling 'tick' function instance from module");
 
-        let frame = self.frame_manager.get_free_frame()?;
+        let mut frame = self.frame_manager.get_free_frame()?;
         let view = self
             .module_instance
             .exports
             .get_memory("image_buffer")?
             .view(&self.wasm_store);
         frame.copy_from_memory(view)?;
+        self.frame_manager.last_updated = Some(frame.clone());
         Ok(())
     }
 }
